75903a132df930cacb1c33177442bdd9c8f0ec07
Ryan Villanueva <ryanvill@gmail.com>
2021-06-04 14:26:51 -0700

    Tweak eslint

--- client/utils/tax.js
+++
@@ -1,9 +0,0 @@
-import moment from 'moment';
-
-export function getRelevantTaxYear() {
-  const isJanuary = moment().month() === 0;
-  const lastYear = moment().year() - 1;
-  const thisYear = moment().year();
-  if(isJanuary) return lastYear;
-  return thisYear;
-}

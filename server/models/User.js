bf0a76e8dd20194625ae49504acae44494b196b5
dodgees <dodgees2@gmail.com>
2021-08-12 09:12:42 -0500

    Adding member count to organizations page, organizations endpoint, and sequalize assocation between User and Organization.

--- server/models/User.js
+++ server/models/User.js
@@ -212,7 +212,7 @@ module.exports = (sequelize, DataTypes) => {
   };
 
   User.associate = function(models) {
-    User.belongsTo(models.Organization, {as: 'Organization', foreignKey: 'organization'});
+    User.belongsTo(models.Organization, {as: 'Organization', sourceKey: 'organization', foreignKey: 'id'});
   };
 
   return User;

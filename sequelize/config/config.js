ca0dcd98d29903c6f56fbb93c9541fb8de813e14
Ryan Villanueva <ryanvill@gmail.com>
2021-11-05 02:03:03 +0000

    Dockerize application

--- sequelize/config/config.js
+++ sequelize/config/config.js
@@ -3,13 +3,14 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
 }
 const parse = require('pg-connection-string').parse;
 const parsed = parse(process.env.DATABASE_URL || '');
-
+console.log(parsed)
 module.exports = {
   development: {
     username: parsed.user || '',
     password: parsed.password || null,
     database: parsed.database || '',
     host: parsed.host || '127.0.0.1',
+    port: parsed.port || '5432',
     dialect: 'postgres',
     define: {
       timestamps: true

24de10f5d1bd5b7bc953819bddf0048e19eb9bfd
Ryan Villanueva <ryanvill@gmail.com>
2021-12-03 14:26:16 +0000

    Update Github Actions

--- server/models/User.js
+++ server/models/User.js
@@ -1,114 +1,132 @@
-'use strict';
+"use strict";
 
-import cryptoUtil from '../utils/crypto';
-import passwordIsValid from '../../shared/validation/password';
-import moment from 'moment';
-import randomstring from 'randomstring';
-import jwt from 'jsonwebtoken';
-import config from '../config/environment';
+import cryptoUtil from "../utils/crypto";
+import passwordIsValid from "../../shared/validation/password";
+import moment from "moment";
+import randomstring from "randomstring";
+import jwt from "jsonwebtoken";
+import config from "../config/environment";
 
-var validatePresenceOf = function(value) {
+var validatePresenceOf = function (value) {
   return value && value.length;
 };
 
 module.exports = (sequelize, DataTypes) => {
-  var User = sequelize.define('user', {
-    id: {
-      primaryKey: true,
-      type: DataTypes.UUID,
-      allowNull: false,
-      defaultValue: DataTypes.UUIDV4
-    },
-    name: {type: DataTypes.STRING, allowNull: false, defaultValue: ''},
-    email: {
-      type: DataTypes.STRING,
-      unique: {
-        msg: 'The specified email address is already in use.'
+  var User = sequelize.define(
+    "user",
+    {
+      id: {
+        primaryKey: true,
+        type: DataTypes.UUID,
+        allowNull: false,
+        defaultValue: DataTypes.UUIDV4,
       },
-      validate: {
-        isEmail: true
-      }
+      firstName: { type: DataTypes.STRING, allowNull: false, defaultValue: "" },
+      lastName: { type: DataTypes.STRING, allowNull: false, defaultValue: "" },
+      email: {
+        type: DataTypes.STRING,
+        unique: {
+          msg: "The specified email address is already in use.",
+        },
+        validate: {
+          isEmail: true,
+        },
+      },
+      role: {
+        type: DataTypes.STRING,
+        defaultValue: "unverified",
+      },
+      password: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: true,
+          isStrong: async (password) => {
+            const response = await passwordIsValid(password);
+            if (!response.isValid) {
+              throw new Error("Password is not strong enough.");
+            }
+          },
+        },
+      },
+      provider: DataTypes.STRING,
+      salt: DataTypes.STRING,
+      passwordResetToken: DataTypes.STRING,
+      passwordResetTokenExpiresAt: DataTypes.DATE,
+      emailVerificationToken: DataTypes.STRING,
+      emailVerificationTokenExpiresAt: DataTypes.DATE,
+      stateOfWork: DataTypes.TEXT,
+      jobTitle: DataTypes.TEXT,
+      organization: {
+        type: DataTypes.UUID,
+        references: {
+          model: "organizations",
+          key: "id",
+        },
+      },
     },
-    role: {
-      type: DataTypes.STRING,
-      defaultValue: 'unverified'
-    },
-    password: {
-      type: DataTypes.STRING,
-      validate: {
-        notEmpty: true,
-        isStrong: async password => {
-          const response = await passwordIsValid(password);
-          if (!response.isValid) {
-            throw new Error('Password is not strong enough.');
-          }
-        }
-      }
-    },
-    provider: DataTypes.STRING,
-    salt: DataTypes.STRING,
-    passwordResetToken: DataTypes.STRING,
-    passwordResetTokenExpiresAt: DataTypes.DATE,
-    emailVerificationToken: DataTypes.STRING,
-    emailVerificationTokenExpiresAt: DataTypes.DATE,
-    stateOfWork: DataTypes.TEXT,
-    jobTitle: DataTypes.TEXT,
-    organization: {
-      type: DataTypes.UUID,
-      references: {
-        model: 'organizations',
-        key: 'id'
-      }
-    },
-  }, {
-    defaultScope: {
-      attributes: { exclude: ['passwordResetToken', 'passwordResetTokenExpiresAt', 'emailVerificationToken', 'emailVerificationTokenExpiresAt', 'salt', 'password'] }
-    },
-    scopes: {
-      withSecrets: {
-        attributes: {}
-      }
-    },
-    /**
-     * Virtual Getters
-     */
-    getterMethods: {
-      // Public profile information
-      profile() {
-        return {
-          name: this.name,
-          role: this.role
-        };
+    {
+      defaultScope: {
+        attributes: {
+          exclude: [
+            "passwordResetToken",
+            "passwordResetTokenExpiresAt",
+            "emailVerificationToken",
+            "emailVerificationTokenExpiresAt",
+            "salt",
+            "password",
+          ],
+        },
+      },
+      scopes: {
+        withSecrets: {
+          attributes: {},
+        },
+      },
+      /**
+       * Virtual Getters
+       */
+      getterMethods: {
+        // Public profile information
+        name() {
+          return `${this.firstName} + ${this.lastName}`;
+        },
+        profile() {
+          return {
+            firstName: this.firstName,
+            lastName: this.lastName,
+            role: this.role,
+          };
+        },
+
+        // Non-sensitive info we'll be putting in the token
+        token() {
+          return {
+            id: this.id,
+            role: this.role,
+          };
+        },
       },
 
-      // Non-sensitive info we'll be putting in the token
-      token() {
-        return {
-          id: this.id,
-          role: this.role
-        };
-      }
-    },
-
-    /**
-     * Pre-save hooks
-     */
-    hooks: {
-      beforeBulkCreate(users) {
-        var promises = users.map(user => user.updatePassword());
-        return Promise.all(promises);
-      },
-      beforeCreate(user) {
-        return user.updatePassword();
-      },
-      beforeUpdate(user) {
-        if (user.changed('password')) {
+      /**
+       * Pre-save hooks
+       */
+      hooks: {
+        beforeBulkCreate(users) {
+          var promises = users.map((user) => user.updatePassword());
+          return Promise.all(promises);
+        },
+        beforeCreate(user) {
           return user.updatePassword();
-        }
-        return Promise.resolve();
-      }
+        },
+        beforeUpdate(user) {
+          if (user.changed("password")) {
+            return user.updatePassword();
+          }
+          return Promise.resolve();
+        },
+      },
     }
-  });
+  );
 
   /**
    * Authenticate - check if the passwords are the same
@@ -118,15 +136,14 @@ module.exports = (sequelize, DataTypes) => {
    * @return {Boolean}
    * @api public
    */
-  User.prototype.authenticate = async function(password) {
+  User.prototype.authenticate = async function (password) {
     const encrypted = await this.encryptPassword(password);
     return this.password === encrypted;
   };
 
-
-  User.prototype.getSignedToken = function() {
-    return jwt.sign({ id: this.get('id') }, config.secrets.session, {
-      expiresIn: 60 * 60 * 5
+  User.prototype.getSignedToken = function () {
+    return jwt.sign({ id: this.get("id") }, config.secrets.session, {
+      expiresIn: 60 * 60 * 5,
     });
   };
 
@@ -138,7 +155,7 @@ module.exports = (sequelize, DataTypes) => {
    * @return {String}
    * @api public
    */
-  User.prototype.makeSalt = function() {
+  User.prototype.makeSalt = function () {
     return cryptoUtil.makeSalt();
   };
 
@@ -150,7 +167,7 @@ module.exports = (sequelize, DataTypes) => {
    * @return {String}
    * @api public
    */
-  User.prototype.encryptPassword = function(password) {
+  User.prototype.encryptPassword = function (password) {
     return cryptoUtil.encryptPassword(password, this.salt);
   };
 
@@ -161,12 +178,12 @@ module.exports = (sequelize, DataTypes) => {
    * @return {String}
    * @api public
    */
-  User.prototype.updatePassword = async function() {
+  User.prototype.updatePassword = async function () {
     // Handle new/update passwords
     if (!this.password) return null;
 
     if (!validatePresenceOf(this.password)) {
-      throw new Error('Invalid password');
+      throw new Error("Invalid password");
     }
 
     // Make salt with a callback
@@ -177,42 +194,53 @@ module.exports = (sequelize, DataTypes) => {
     return;
   };
 
-  User.prototype.generatePasswordResetToken = async function() {
-    this.set('passwordResetToken', randomstring.generate({length: 18}));
-    this.set('passwordResetTokenExpiresAt', moment().add(1, 'hour').toDate());
+  User.prototype.generatePasswordResetToken = async function () {
+    this.set("passwordResetToken", randomstring.generate({ length: 18 }));
+    this.set("passwordResetTokenExpiresAt", moment().add(1, "hour").toDate());
     return;
   };
 
-  User.prototype.verifyPasswordResetToken = function(token) {
-    const passwordResetToken = this.get('passwordResetToken');
+  User.prototype.verifyPasswordResetToken = function (token) {
+    const passwordResetToken = this.get("passwordResetToken");
     const matches = passwordResetToken && passwordResetToken === token;
-    const isExpired = moment(this.get('passwordResetTokenExpiresAt')).isBefore(moment());
+    const isExpired = moment(this.get("passwordResetTokenExpiresAt")).isBefore(
+      moment()
+    );
     if (matches && !isExpired) return true;
     return false;
   };
 
-  User.prototype.generateEmailVerificationToken = function() {
-    this.set('emailVerificationToken', randomstring.generate({length: 18}));
-    this.set('emailVerificationTokenExpiresAt', moment().add(6, 'hour').toDate());
+  User.prototype.generateEmailVerificationToken = function () {
+    this.set("emailVerificationToken", randomstring.generate({ length: 18 }));
+    this.set(
+      "emailVerificationTokenExpiresAt",
+      moment().add(6, "hour").toDate()
+    );
   };
 
-  User.prototype.verifyEmailVerificationToken = function(token) {
-    const emailVerificationToken = this.get('emailVerificationToken');
+  User.prototype.verifyEmailVerificationToken = function (token) {
+    const emailVerificationToken = this.get("emailVerificationToken");
     const matches = emailVerificationToken && emailVerificationToken === token;
-    const isExpired = moment(this.get('emailVerificationTokenExpiresAt')).isBefore(moment());
+    const isExpired = moment(
+      this.get("emailVerificationTokenExpiresAt")
+    ).isBefore(moment());
     if (matches && !isExpired) return true;
     return false;
   };
 
-  User.prototype.verify = async function() {
-    this.set('role', 'user');
-    this.set('emailVerificationToken', null);
-    this.set('emailVerificationTokenExpiresAt', null);
+  User.prototype.verify = async function () {
+    this.set("role", "user");
+    this.set("emailVerificationToken", null);
+    this.set("emailVerificationTokenExpiresAt", null);
     await this.save();
   };
 
-  User.associate = function(models) {
-    User.hasOne(models.Organization, {as: 'Organization', sourceKey: 'organization', foreignKey: 'id'});
+  User.associate = function (models) {
+    User.hasOne(models.Organization, {
+      as: "Organization",
+      sourceKey: "organization",
+      foreignKey: "id",
+    });
   };
 
   return User;

0e6da979a186b68bed46753c72e4375360b0ae0e
Ryan Villanueva <ryanvill@gmail.com>
2021-12-21 05:04:55 +0000

    Fix migration

--- sequelize/migrations/20210922055061-repair-job-title.js
+++ sequelize/migrations/20210922055061-repair-job-title.js
@@ -3,13 +3,19 @@
 module.exports = {
   up: async (queryInterface, DataTypes) => {
     await queryInterface.sequelize.query(
-      `update users set "occupation" = "jobTitle" where "createdAt" < '2020-12-04' :: date`
+      `update users set "occupation" = "jobTitle" where "createdAt" < '2021-12-04' :: date`
+    );
+    await queryInterface.sequelize.query(
+      `update users set "jobTitle" = '' where "createdAt" < '2021-12-04' :: date`
     );
   },
 
   down: async (queryInterface) => {
     await queryInterface.sequelize.query(
-      `update users set "jobTitle" = "occupation" where "createdAt" < '2020-12-04' :: date`
+      `update users set "jobTitle" = "occupation" where "createdAt" < '2021-12-04' :: date`
+    );
+    await queryInterface.sequelize.query(
+      `update users set "occupation" = '' where "createdAt" < '2021-12-04' :: date`
     );
   },
 };

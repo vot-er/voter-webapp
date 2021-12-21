6b451267c012db6f58871dd6f1a67954cae22e18
Ryan Villanueva <ryanvill@gmail.com>
2021-12-21 03:53:57 +0000

    Update .env loader

--- sequelize/config/config.js
+++ sequelize/config/config.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config({ path: "../.env" });
 const parse = require("pg-connection-string").parse;
 const parsed = parse(process.env.DATABASE_URL || "");
 
--- server/routes.js
+++ server/routes.js
@@ -2,36 +2,35 @@
  * Main application routes
  */
 
-'use strict';
+"use strict";
 
-import errors from './components/errors';
-import path from 'path';
-import cors from 'cors';
+import errors from "./components/errors";
+import path from "path";
+import cors from "cors";
 
-
-export default function(app) {
+export default function (app) {
   // Insert routes below
 
-  app.use('/api/users', require('./api/user'));
-  app.use('/api/kits', require('./api/kit'));
-  app.use('/api/organizations', require('./api/organization'));
+  app.use("/api/users", require("./api/user"));
+  app.use("/api/kits", require("./api/kit"));
+  app.use("/api/organizations", require("./api/organization"));
 
-  app.options('/api/events', cors());
-  app.use('/api/events', cors(), require('./api/event'));
+  app.options("/api/events", cors());
+  app.use("/api/events", cors(), require("./api/event"));
 
-  app.use('/auth', require('./auth').default);
+  app.use("/auth", require("./auth").default);
 
   // All undefined asset or api routes should return a 404
-  app.route('/:url(api|auth|components|app|bower_components|assets)/*')
+  app
+    .route("/:url(api|auth|components|app|bower_components|assets)/*")
     .get(errors[404]);
 
   // All other routes should redirect to the app.html
-  app.route('/*')
-    .get((req, res) => {
-      if (app.get('env') === 'production') {
-        res.render(path.resolve(`${app.get('appPath')}/index.html`));
-      } else {
-        res.render(path.resolve(`${app.get('appPath')}/index.js`));
-      }
-    });
+  app.route("/*").get((req, res) => {
+    if (app.get("env") === "production") {
+      res.render(path.resolve(`${app.get("appPath")}/index.html`));
+    } else {
+      res.render(path.resolve(`${app.get("appPath")}/index.js`));
+    }
+  });
 }

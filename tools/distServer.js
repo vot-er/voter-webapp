557a1a1da4a2e3be2235bf0c38fda356ada08cdc
Ryan Villanueva <ryanvill@gmail.com>
2021-12-21 01:47:03 +0000

    Update environment

--- tools/distServer.js
+++ tools/distServer.js
@@ -1,18 +1,15 @@
 // This file configures a web server for testing the production build
 // on your local machine.
+require("dotenv").config();
 
-var nodemon = require('nodemon');
-var path = require('path');
+var nodemon = require("nodemon");
+var path = require("path");
 
-const localEnvObj = require('../server/config/local.env.js');
-Object.keys(localEnvObj).forEach(key => {
-  process.env[key] = String(localEnvObj[key]);
-});
 /* eslint-disable no-console */
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'production';
-var serverPath = path.join(__dirname, '../dist/server');
+process.env.NODE_ENV = process.env.NODE_ENV || "production";
+var serverPath = path.join(__dirname, "../dist/server");
 
-console.log('Opening production build...');
+console.log("Opening production build...");
 
 nodemon(`-w ${serverPath} ${serverPath}`);
